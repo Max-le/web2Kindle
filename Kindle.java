2403f029ee1d0828ffe62af8741a790f9918a3f3
Max-le <max.09@outlook.com>
2018-10-07 18:41:28 +0200

    first commit 😃

---
+++ Kindle.java
@@ -0,0 +1,19 @@
+import java.lang.Object;
+
+
+
+public class Kindle{
+
+
+	public static void main(String[] args) {
+	Rssfile myrss = new Rssfile() ; 
+
+	 myrss.adress = "https://www.lemonde.fr/pixels/rss_full.xml";
+	 myrss.checkAdress();
+
+	}
+}
+
+
+
+ 
---
+++ Rssfile.java
@@ -0,0 +1,29 @@
+import java.net.URL;
+import java.net.MalformedURLException;
+
+public class Rssfile {
+
+
+	 String adress;
+
+	public void checkAdress(){
+		 
+	try
+	{ URL myURL = new URL("http//lemonde.fr/");
+}
+	
+
+
+			catch (MalformedURLException e) {
+   System.out.println("Erreur ! ");
+}
+
+
+		return;
+
+
+	}
+
+
+
+}
