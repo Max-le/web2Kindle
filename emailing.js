94180c0e1766fe0ecdfc2497ef86834d82d3f400
Max <max.09@outlook.com>
2019-05-14 15:26:26 +0200

    email settings ready

--- emailing.js
+++ emailing.js
@@ -1,11 +1,29 @@
 const nodemailer = require("nodemailer");
+const credentials = require('./credentials');
 
-nodemailer.createTransport({
+let transporter = nodemailer.createTransport({
     host: "ssl0.ovh.net",
     port: 587,
     secure: false, // upgrade later with STARTTLS
     auth: {
-      user: "info@qonnect.fr",
-      pass: "pasfy3RBkgk78sword"
+      user: credentials.user,
+      pass: credentials.password
     }
   });
+
+  transporter.verify(function(error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
+  var message = {
+    from: 'info@qonnect.fr',
+    to: 'max.09@outlook.com',
+    subject: 'Message title',
+    text: 'Plaintext version of the message',
+    html: '<p>HTML version of the message</p>'
+};
+transporter.sendMail(message)
